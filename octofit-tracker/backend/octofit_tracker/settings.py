ca680a9d2e147d9ba5a26fbb4d83eb023ab46551
AccentureAgent <sahil.sudama.jadhav@accenture.com>
2026-01-02 09:39:49 +0000

    Update ALLOWED_HOSTS and api_root to support Codespaces environment

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 # Add required apps for Octofit Tracker
 INSTALLED_APPS += [
